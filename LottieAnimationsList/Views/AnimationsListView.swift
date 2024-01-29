1aca854b9c4460f6d8f3a88295dfb5602818e594
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-29 09:11:56 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by 宮川義之助 on 2024/01/29.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
