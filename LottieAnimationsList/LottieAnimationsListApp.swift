a993bfbf53d7f1bb41209983a0d9c84ecc15db46
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-29 09:03:12 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by 宮川義之助 on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
